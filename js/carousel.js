a5ea2391b809c169141a2611dcfe9e1e7335e47f
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-04-24 21:29:40 +0200

    add closure with carousel function

--- js/carousel.js
+++ js/carousel.js
@@ -1,24 +1,77 @@
-function renderCarouselSlides() {
-  document.querySelector(".slides--container").innerHTML = `
- <div class="slides">
-<div class="slides__img"> <img src="" alt=""></div>
- </div>
- <div><p class="slides__tag--featured"></p><h2 class="slides__h2"></h2> <p class="slides__p--breadtext"></p></div>
+const [increment, decrement, changeSlides] = carouselState();
+
+fetch("http://jarleblog.local//wp-json/wp/v2/posts/?_embed").then((res) =>
+  res.json().then((data) => {
+    console.log(data);
+    const parsedPosts = processResponse(data);
+    parsedPosts.forEach((element) => renderCarouselSlides(element));
+    console.log(parsedPosts);
+    const slidesArray = document.querySelectorAll(".slides");
+    console.log(slidesArray);
+    slidesArray[0].classList.add("active");
+    document
+      .querySelector("#forward--button")
+      .addEventListener("click", () => changeSlides(increment, slidesArray));
+    document
+      .querySelector("#backward--button")
+      .addEventListener("click", () => changeSlides(decrement, slidesArray));
+  })
+);
+
+function renderCarouselSlides({ title, text, imgURL, date, modified }) {
+  document.querySelector(".slides--container").innerHTML += `
+ <div class="slides" style="background-image:url(${imgURL}); background-size:cover;">
+
+ <div><p class="slides__tag--featured">Featured</p><h2 class="slides__h2">${title}</h2> <p class="slides__p--breadtext">${text}</p></div>
    `;
 }
 
-function changeSlides(slidesArray) {}
-
 function carouselState() {
-  indexCount = 0;
-  function increment() {
+  let indexCount = 0;
+  function increment(arr) {
+    if (arr) {
+      if (indexCount >= arr.length - 1) {
+        return (indexCount = 0);
+      }
+    }
+    console.log(indexCount + 1);
     return indexCount++;
   }
-  function decrement() {
+  function decrement(arr) {
+    if (arr) {
+      if (indexCount === 0) {
+        return (indexCount = arr.length - 1);
+      }
+    }
+    console.log(indexCount - 1);
     return indexCount--;
   }
 
-  return [increment, decrement, indexCount];
+  function changeSlides(callback, arr) {
+    console.log(indexCount);
+    arr[indexCount].classList.remove("active");
+    callback(arr);
+    arr[indexCount].classList.add("active");
+  }
+
+  return [increment, decrement, changeSlides];
 }
 
-function changeSlides(callback) {}
+function processResponse(arr) {
+  const newArr = [];
+  arr.forEach((element) => {
+    const newObject = {};
+    newObject.title = element.title.rendered;
+    //regex gotten from chatGPT
+    newObject.text = element.excerpt.rendered.replace(/(<p>|<\/p>|\n)/g, "");
+    newObject.date = element.date;
+    newObject.edited = element.modified;
+    newObject.imgURL = element._embedded["wp:featuredmedia"]
+      ? element._embedded["wp:featuredmedia"][0].media_details.sizes.full
+          .source_url
+      : "";
+
+    newArr.push(newObject);
+  });
+  return newArr;
+}
