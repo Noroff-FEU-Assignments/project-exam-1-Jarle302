db6cc083933cf774ca546903face6fe036049f64
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-04-26 12:09:36 +0200

    make functions exports

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,10 @@
 const [increment, decrement, changeSlides] = carouselState();
-import { username, password } from "./imports.js";
+import {
+  username,
+  password,
+  renderBlogPosts,
+  processResponse,
+} from "./imports.js";
 fetch("https://jarleblogg.no/wp-json/wp/v2/posts/?_embed", {
   method: "GET",
   headers: { Authorization: "Basic " + btoa(`${username}:${password}`) },
@@ -7,7 +12,7 @@ fetch("https://jarleblogg.no/wp-json/wp/v2/posts/?_embed", {
   res.json().then((data) => {
     console.log(data);
     const parsedPosts = processResponse(data);
-    parsedPosts.forEach((element) => renderCarouselSlides(element));
+    parsedPosts.forEach((element) => renderBlogPosts(element));
     console.log(parsedPosts);
     const slidesArray = document.querySelectorAll(".slides");
     console.log(slidesArray);
@@ -20,14 +25,6 @@ fetch("https://jarleblogg.no/wp-json/wp/v2/posts/?_embed", {
       .addEventListener("click", () => changeSlides(decrement, slidesArray));
   })
 );
-
-function renderCarouselSlides({ title, text, imgURL, date, modified, id }) {
-  document.querySelector(".slides--container").innerHTML += `
- <a href="/html/blogDetails.html?id=${id}" class="slides" style="background-image:url(${imgURL}); background-size:cover;">
-
- <div class="slides--container--textbox"><p class="carousel--date">${date}</p><p class="slides__tag--featured">Latest</p><h2 class="slides__h2">${title}</h2> <p class="slides__p--breadtext">${text}</p></div>
-  </a> `;
-}
 
 function carouselState() {
   let indexCount = 0;
@@ -58,24 +55,4 @@ function carouselState() {
   }
 
   return [increment, decrement, changeSlides];
-}
-
-function processResponse(arr) {
-  const newArr = [];
-  arr.forEach((element) => {
-    const newObject = {};
-    newObject.title = element.title.rendered;
-    //regex gotten from chatGPT
-    newObject.id = element.id;
-    newObject.text = element.excerpt.rendered.replace(/(<p>|<\/p>|\n)/g, "");
-    newObject.date = element.date;
-    newObject.edited = element.modified;
-    newObject.imgURL = element._embedded["wp:featuredmedia"]
-      ? element._embedded["wp:featuredmedia"][0].media_details.sizes.full
-          .source_url
-      : "";
-
-    newArr.push(newObject);
-  });
-  return newArr;
 }
--- js/imports.js
+++ js/imports.js
@@ -1,2 +1,30 @@
 export const username = "jarlehtollaksen2@live.no";
 export const password = `5PWI pcln 2hoE 1Tid H0Nn UfOl`;
+
+export function renderBlogPosts({ title, text, imgURL, date, modified, id }) {
+  document.querySelector(".slides--container").innerHTML += `
+   <a href="/html/blogDetails.html?id=${id}" class="slides" style="background-image:url(${imgURL}); background-size:cover;">
+  
+   <div class="slides--container--textbox"><p class="carousel--date">${date}</p><p class="slides__tag--featured">Latest</p><h2 class="slides__h2">${title}</h2> <p class="slides__p--breadtext">${text}</p></div>
+    </a> `;
+}
+
+export function processResponse(arr) {
+  const newArr = [];
+  arr.forEach((element) => {
+    const newObject = {};
+    newObject.title = element.title.rendered;
+    //regex gotten from chatGPT
+    newObject.id = element.id;
+    newObject.text = element.excerpt.rendered.replace(/(<p>|<\/p>|\n)/g, "");
+    newObject.date = element.date;
+    newObject.edited = element.modified;
+    newObject.imgURL = element._embedded["wp:featuredmedia"]
+      ? element._embedded["wp:featuredmedia"][0].media_details.sizes.full
+          .source_url
+      : "";
+
+    newArr.push(newObject);
+  });
+  return newArr;
+}
