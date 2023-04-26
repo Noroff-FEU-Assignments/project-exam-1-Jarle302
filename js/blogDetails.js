1ec7cdf6c5fcfcd9c8082c2e3af76bf68920d980
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-04-26 11:59:16 +0200

    add render blog post on details page

--- js/blogDetails.js
+++ js/blogDetails.js
@@ -6,4 +6,12 @@ const blogID = parameters.get("id");
 fetch(`https://jarleblogg.no/wp-json/wp/v2/posts/${blogID}`, {
   method: "GET",
   headers: { Authorization: "Basic " + btoa(`${username}:${password}`) },
-}).then((res) => res.json().then((data) => console.log(data)));
+}).then((res) =>
+  res
+    .json()
+    .then(
+      (data) =>
+        (document.querySelector(".blog__div--container").innerHTML =
+          data.content.rendered)
+    )
+);
