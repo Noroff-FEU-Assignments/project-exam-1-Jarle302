d93ecb0fc9b36da0c10d91146bd8ad0104838707
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-04-27 12:50:07 +0200

    add

--- js/blogList.js
+++ js/blogList.js
@@ -6,6 +6,8 @@ import {
   baseURL,
 } from "./imports.js";
 
+fetchBlogposts("10");
+/*
 fetch(`${baseURL}?_embed`, {
   method: "GET",
   headers: { Authorization: "Basic " + btoa(`${username}:${password}`) },
@@ -21,3 +23,35 @@ fetch(`${baseURL}?_embed`, {
       )
     )
 );
+
+*/
+document
+  .querySelector(".blog-list__button--view-more")
+  .addEventListener("click", () => {
+    let counter = 10;
+    let perPage = 100;
+    fetchBlogposts(perPage, counter), (counter += perPage);
+  });
+
+function fetchBlogposts(postsPerPage, offset) {
+  fetch(
+    `${baseURL}?_embed&&?order=desc&?orderby=dateposts?per_page=${postsPerPage}${
+      offset ? `&offset=${offset}` : ""
+    }`,
+    {
+      method: "GET",
+      headers: { Authorization: "Basic " + btoa(`${username}:${password}`) },
+    }
+  ).then((res) =>
+    res
+      .json()
+      .then((data) =>
+        processResponse(data).forEach((element) =>
+          renderBlogPosts(
+            element,
+            document.querySelector(".blog-list__section--container", false)
+          )
+        )
+      )
+  );
+}
