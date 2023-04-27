803a5e8e53ffe594be40761aa78c7badbd49f9b0
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-04-27 20:26:04 +0200

    add img modal

--- js/blogDetails.js
+++ js/blogDetails.js
@@ -7,11 +7,21 @@ fetch(`${baseURL}${blogID}`, {
   method: "GET",
   headers: { Authorization: "Basic " + btoa(`${username}:${password}`) },
 }).then((res) =>
-  res
-    .json()
-    .then(
-      (data) =>
-        (document.querySelector(".blog__div--container").innerHTML =
-          data.content.rendered)
-    )
+  res.json().then((data) => {
+    document.querySelector(".blog__div--container").innerHTML =
+      data.content.rendered;
+    document.querySelectorAll("img").forEach((img) =>
+      img.addEventListener("click", () => {
+        console.log({ img });
+        document.querySelector(".img-modal").style.display = "block";
+        document.querySelector(".img-modal__img").src = img.src;
+      })
+    );
+  })
 );
+
+document
+  .querySelector(".img-modal__button--exit")
+  .addEventListener("click", () => {
+    document.querySelector(".img-modal").style.display = "none";
+  });
