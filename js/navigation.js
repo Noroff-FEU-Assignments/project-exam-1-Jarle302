114bf64647c4babf4a2f09db073b888b3024c27b
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-05-01 13:46:24 +0200

    fix style

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,7 @@
 function renderNavigation() {
   document.querySelector("header").innerHTML = `<nav>
+  <img class="logo-image" src="/assets/logoimage.png" alt="" />
+
     <ul class="nav__ul">
         <li class="nav_ul_li"><a class="nav__ul__li__a" href="/index.html">Home</a></li>
         <li class="nav_ul_li"><a class="nav__ul__li__a" href="/html/blogList.html">Blog</a></li>
