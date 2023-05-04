ebfa0adcc63eaabbbd9c4711f0213c811210f2db
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-05-04 18:59:08 +0200

    add form validation contactpage

---
+++ js/contact.js
@@ -0,0 +1,23 @@
+import { validateInput } from "./imports.js";
+
+validateInput(
+  (value) => value.trim().length > 5,
+  document.querySelector("#input--name"),
+  "Name must be atleast 6 characters"
+);
+
+validateInput(
+  (value) => /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value),
+  document.querySelector("#input--email"),
+  "Please input a valid email address"
+);
+validateInput(
+  (value) => value.trim().length > 15,
+  document.querySelector("#input--subject"),
+  "Subject needs to be atleast 16 characters"
+);
+validateInput(
+  (value) => value.trim().length > 25,
+  document.querySelector("#input--message"),
+  "Subject needs to be atleast 26 characters"
+);
--- js/imports.js
+++ js/imports.js
@@ -40,3 +40,17 @@ export function processResponse(arr) {
   });
   return newArr;
 }
+
+export function validateInput(
+  callback,
+  domEl,
+  errMessage,
+  succMessage = ` <p class="success-message" ><i class="fa-regular fa-square-check";"></i> Done <p>`
+) {
+  const errorDiv = document.createElement("span");
+  errorDiv.classList.add("error");
+  domEl.insertAdjacentElement("afterEnd", errorDiv);
+  domEl.addEventListener("blur", () => {
+    errorDiv.innerHTML = callback(domEl.value) ? succMessage : errMessage;
+  });
+}
