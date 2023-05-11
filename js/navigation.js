0cdee8052a0227e345c86bd9da6ecdb7d6f6c55a
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-05-11 19:40:07 +0200

    add

--- js/navigation.js
+++ js/navigation.js
@@ -4,7 +4,7 @@ function renderNavigation() {
 
     <ul class="nav__ul">
         <li class="nav_ul_li"><a class="nav__ul__li__a" href="/index.html">Home</a></li>
-        <li class="nav_ul_li"><a class="nav__ul__li__a" href="/html/blogList.html">Blog</a></li>
+        <li class="nav_ul_li"><a class="nav__ul__li__a" href="/html/bloglist.html">Blog</a></li>
         <li class="nav_ul_li"><a class="nav__ul__li__a" href="/html/about.html">About</a></li>
         <li class="nav_ul_li"><a class="nav__ul__li__a" href="/html/contact.html">Contact</a></li>
     </ul>
