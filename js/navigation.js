cd8387acf069f8d275e4fb1199333a6090855931
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-05-11 19:16:57 +0200

    add

--- js/navigation.js
+++ js/navigation.js
@@ -19,10 +19,10 @@ document
 
 function isActive(element) {
   console.log(
-    element.href.split("/")[element.href.split("/").length - 1],
+    element.href.split("/")[element.href.split("/").length - 1].split(".")[0],
     window.location.pathname.split("/")[0]
   );
-  return window.location.pathname.split("/")[1] ===
+  return window.location.pathname.split("/")[0] ===
     element.href.split("/")[element.href.split("/").length - 1].split(".")[0]
     ? true
     : false;
