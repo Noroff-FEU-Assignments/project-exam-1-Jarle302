7729632f2881a105e7a24c699fb52affba40d015
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-05-11 20:06:53 +0200

    add

--- js/navigation.js
+++ js/navigation.js
@@ -2,7 +2,7 @@ function renderNavigation() {
   document.querySelector("header").innerHTML = `<nav>
   <a href="/index.html"> <img class="logo-image" src="/assets/logo.png" alt="" /> </a>
 
-    <ul class="nav__ul">
+    <ul class="nav__ul wrapper">
         <li class="nav_ul_li"><a class="nav__ul__li__a" href="/index.html">Home</a></li>
         <li class="nav_ul_li"><a class="nav__ul__li__a" href="/html/bloglist.html">Blog</a></li>
         <li class="nav_ul_li"><a class="nav__ul__li__a" href="/html/about.html">About</a></li>
