8087a410504113e5393f89dbdd8435e5d57b696e
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-05-13 13:34:25 +0200

    add

--- js/blogList.js
+++ js/blogList.js
@@ -12,6 +12,7 @@ fetchBlog();
 document
   .querySelector(".blog-list__button--view-more")
   .addEventListener("click", () => {
+    document.querySelector(".spinnerTwo").style.display = "block";
     fetchBlog();
   });
 
